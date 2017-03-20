76e47e39a2fe14a59ceb96f304afe56fdfdff8bc
KangJiYoung <teucas@yahoo.com>
2017-03-20 18:44:32 +0200

    Implement Tile Manager

--- Screens/Game/BattleScreen.cs
+++ Screens/Game/BattleScreen.cs
@@ -1,17 +1,44 @@
 ﻿using Heroes3.Data;
 using Heroes3.Screens.Base;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Heroes3.Screens.Game
 {
     public class BattleScreen : GameScreen
     {
-        private Faction player1Faction;
-        private Faction player2Faction;
+        private Faction player1Faction, player2Faction;
+
+        private Texture2D battleBackgorund;
 
         public BattleScreen(Faction player1Faction, Faction player2Faction)
         {
             this.player1Faction = player1Faction;
             this.player2Faction = player2Faction;
+        }
+
+        public override void LoadContent()
+        {
+            ScreenManager.Game.Components.Add(new TileManager(ScreenManager.Game));
+
+            var content = ScreenManager.Game.Content;
+
+            battleBackgorund = content.Load<Texture2D>("Images/Game/Battle/BattleBackground");
+
+            base.LoadContent();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            var spriteBatch = ScreenManager.SpriteBatch;
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(battleBackgorund, new Vector2(0, 0), Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
         }
     }
 }
---
+++ TileManager.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Heroes3
+{
+    public class TileManager : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+
+        private Texture2D tileTexture;
+
+        public TileManager(Game game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var content = Game.Content;
+
+            tileTexture = content.Load<Texture2D>("Images/Game/Battle/TileTexture");
+
+            base.Initialize();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+
+            var initialYoffset = 180;
+            var initialXoffset = 80;
+            for (int i = 0; i < 10; i++)
+            {
+                var currentXoffset = initialXoffset;
+
+                for (int j = 0; j < 23; j++)
+                {
+                    spriteBatch.Draw(tileTexture, new Vector2(currentXoffset + j * 45, initialYoffset + i * 45), Color.White);
+
+                    currentXoffset += 4;
+                }
+
+                initialYoffset += 4;
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
